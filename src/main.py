070828a9144acaaadeaaa9499cfbc29542b064e7
i13302 <s16t286@stu.kagawa-u.ac.jp>
2019-03-08 19:51:37 +0900

    iso-2022-jpでは読み取れないものがある

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@
 #                       Nov/26/2017
 #
 # --------------------------------------------------------------------
+import re
 import sys
 import os
 import imaplib
@@ -27,33 +28,49 @@ try:
     Mm = imaplib.IMAP4_SSL(server)
     Mm.login(user, password)
     Mm.select()
-    typ, data = Mm.search(None, 'ALL')
+    typ, data = Mm.search(None, 'SEEN')
     mailIdList = data[0].split()
     mailIdList.reverse()
     for num in mailIdList:
+        # num = b'9609'
         print(num)
         typ, data = Mm.fetch(num, '(RFC822)')
+
+        tmp_msg = email.message_from_string(
+            data[0][1].decode('utf-8'))
+        tmp_subject = tmp_msg.get('Subject')
+
+        if re.match(tmp_subject, 'utf-8'):
+            encoding = 'utf-8'
+        else:
+            encoding = 'iso-2022-jp-ext'
+            # encoding = 'shift_jis'
+
+        print(encoding)
         email_message = email.message_from_string(
-            data[0][1].decode('iso-2022-jp'))
-        print(email_message)
-        break
-# #
-# 		email_from = str(make_header(decode_header(email_message['From'])))
-# 		print(email_from)
-# 		subject = str(make_header(decode_header(email_message['Subject'])))
-# 		print(subject)
+            data[0][1].decode(encoding))
+        # break
 
-# 		msg_encoding = 'iso-2022-jp'
+        email_from = str(make_header(decode_header(email_message['From'])))
+        print(email_from)
 
-# 		if email_message.is_multipart() == False: # シングルパート
-# 			byt  = bytearray(email_message.get_payload(), msg_encoding)
-# 			body = byt.decode(encoding=msg_encoding)
-# 		else:   # マルチパート
-# 			prt  = email_message.get_payload()[0]
-# 			byt  = prt.get_payload(decode=True)
-# 			body = byt.decode(encoding=msg_encoding)
-# #
-        # print(body)
+        try:
+            subject = str(make_header(decode_header(email_message['Subject'])))
+            print(subject)
+        except Exception as e1:
+            # print(email_message['Subject'].encode('shift_jis').decode('iso-2022-jp-ext'))
+            print("[CATCH ERROR]")
+
+#         if email_message.is_multipart() == False:  # シングルパート
+#             byt = bytearray(email_message.get_payload(), encoding)
+#             body = byt.decode(encoding=encoding)
+#         else:   # マルチパート
+#             prt = email_message.get_payload()[0]
+#             byt = prt.get_payload(decode=True)
+#             body = byt.decode(encoding=encoding)
+# # #
+#         print(body)
+        # break
 
     Mm.close()
     Mm.logout()
